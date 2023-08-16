48766ea49e951f077c9a48b36b882c80d5d97334
tylersmith2614 <tylersmith2614@gmail.com>
2023-08-16 13:36:08 -0600

    testing bug in home-routes.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,50 +3,32 @@ const router = require("express").Router();
 const { Post, User } = require("../models");
 const Comment = require("../models/comment");
 
-router.get("/", async (req, res) => {
-  try {
-    const dbPostData = await Post.findAll({
-      attributes: ["id", "title", "content", "created_at", "user_id"],
-      include: [
-        {
-          model: Comment,
-          attributes: [
-            "id",
-            "comment_text",
-            "post_id",
-            "user_id",
-            "created_at",
-          ],
-          include: [
-            {
-              model: User,
-              attributes: ["username"],
-            },
-          ],
-        },
-        {
+router.get("/", (req, res) => {
+  Post.findAll({
+    attributes: ["id", "title", "content", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
           model: User,
           attributes: ["username"],
         },
-      ],
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("homepage", { posts, loggedIn: req.session.loggedIn });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     });
-
-    console.log(dbPostData);
-
-    // Serialize the data, essentially making it an easier object to iterate through
-    const posts = dbPostData.map((post) => post.get({ plain: true }));
-    // Reverse the order of the posts array so that the most recent post is first
-    posts.reverse();
-    // Render the homepage template and include the posts object we just declared
-    res.render("homepage", {
-      posts,
-      loggedIn: req.session.loggedIn,
-      username: req.session.username,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
 });
 
 router.get("/login", (req, res) => {
@@ -61,81 +43,75 @@ router.get("/signup", (req, res) => {
   res.render("signup");
 });
 
-router.get("/post/:id", async (req, res) => {
-  try {
-    const dbPostData = await Post.findOne({
-      where: {
-        id: req.params.id,
+router.get("/post/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "content", "title", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
       },
-      attributes: ["id", "content", "title", "created_at"],
-      include: [
-        {
-          model: Comment,
-          attributes: [
-            "id",
-            "comment_text",
-            "post_id",
-            "user_id",
-            "created_at",
-          ],
-          include: {
-            model: User,
-            attributes: ["username"],
-          },
-        },
-        {
-          model: User,
-          attributes: ["username"],
-        },
-      ],
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+      const post = dbPostData.get({ plain: true });
+      console.log(post);
+      res.render("single-post", { post, loggedIn: req.session.loggedIn });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     });
-
-    if (!dbPostData) {
-      res.status(404).json({ message: "No post found with this id" });
-      return;
-    }
-
-    const post = dbPostData.get({ plain: true });
-    console.log(post);
-    res.render("single-post", { post, loggedIn: req.session.loggedIn });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
 });
-
-router.get("/posts-comments", async (req, res) => {
-  try {
-    const dbPostData = await Post.findAll({
-      attributes: ["id", "content", "title", "created_at"],
-      include: [
-        {
-          model: Comment,
-          attributes: [
-            "id",
-            "comment_text",
-            "post_id",
-            "user_id",
-            "created_at",
-          ],
-          include: {
-            model: User,
-            attributes: ["username"],
-          },
-        },
-        {
+router.get("/posts-comments", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "content", "title", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
           model: User,
           attributes: ["username"],
         },
-      ],
-    });
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+      const post = dbPostData.get({ plain: true });
 
-    const posts = dbPostData.map((post) => post.get({ plain: true }));
-    res.render("posts-comments", { posts, loggedIn: req.session.loggedIn });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
+      res.render("posts-comments", { post, loggedIn: req.session.loggedIn });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
