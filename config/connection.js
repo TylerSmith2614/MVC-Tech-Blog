fa598c7b51fb5d41c350656a11456ed27964d606
tylersmith2614 <tylersmith2614@gmail.com>
2023-08-16 01:07:40 -0600

    updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,21 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+const Sequelize = require("sequelize");
+require("dotenv").config();
 
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
+  // Use JAWSDB_URL from environment variables for deployment on Railway
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
+  // Use environment variables for local development
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
+      host: process.env.DB_HOST || "localhost", // Use the DB_HOST environment variable or default to 'localhost'
+      dialect: "mysql",
+      port: process.env.DB_PORT || 3306, // Use the DB_PORT environment variable or default to 3306
     }
   );
 }
